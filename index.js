10099d41506e08ff922c9eb43a6c19681dc91732
YounesAEO <y.aboueleomoum09@gmail.com>
2023-02-12 14:12:15 +0100

    add logic to extract and store text from cv

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const checkForKey = () => {
+import { Pdf2TextClass } from './pdf2text.js';
+const checkForKey = (key) => {
 	return new Promise((resolve, reject) => {
-		chrome.storage.local.get(['openai-key'], (result) => {
-			resolve(result['openai-key']);
+		chrome.storage.local.get([key], (result) => {
+			resolve(result[key]);
 		});
 	});
 };
@@ -31,14 +32,88 @@ const changeKey = () => {
 	document.getElementById('key_entered').style.display = 'none';
 };
 
+const saveCvURL = () => {
+	const files = document.getElementById('cv_file').files;
+	if (files.length > 0) {
+		//we get the file
+		const file = files[0];
+		const reader = new FileReader();
+		const pdff = new Pdf2TextClass();
+		//we save it's url in chrome storage
+		reader.onload = (e) => {
+			let cvContent = '';
+			pdff.pdfToText(e.target.result, null, (text) => {
+				cvContent = text;
+			});
+			document.getElementById('loading').style.display = 'block';
+			document.getElementById('cv_needed').style.display = 'none';
+			window.setTimeout(async () => {
+				const encodedURL = encode(e.target.result);
+				const encodedText = encode(cvContent);
+				const result = await chrome.storage.local.set(
+					{ 'cv-content': encodedText, 'cv-url': encodedURL },
+					() => {
+						document.getElementById('loading').style.display =
+							'none';
+						document.getElementById('cv_entered').style.display =
+							'block';
+						document
+							.getElementById('cv')
+							.addEventListener('click', (ev) => {
+								ev.preventDefault();
+								window
+									.open()
+									.document.write(
+										`<iframe src="${e.target.result}" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; margin:0;" allowfullscreen></iframe>`
+									);
+							});
+					}
+				);
+			}, 1500);
+		};
+		reader.readAsDataURL(file);
+	}
+};
+
+const changeCvURL = () => {
+	document.getElementById('cv_needed').style.display = 'block';
+	document.getElementById('cv_entered').style.display = 'none';
+};
+
 document.getElementById('save_key_button').addEventListener('click', saveKey);
 document
 	.getElementById('change_key_button')
 	.addEventListener('click', changeKey);
 
-checkForKey().then((response) => {
+document
+	.getElementById('change_cv_button')
+	.addEventListener('click', changeCvURL);
+
+document.getElementById('cv_file').addEventListener('change', saveCvURL, false);
+
+checkForKey('openai-key').then((response) => {
 	if (response) {
 		document.getElementById('key_needed').style.display = 'none';
 		document.getElementById('key_entered').style.display = 'block';
+		document
+			.getElementById('key_input')
+			.setAttribute('value', atob(response));
+	}
+});
+
+checkForKey('cv-url').then((response) => {
+	if (response) {
+		document.getElementById('cv_needed').style.display = 'none';
+		document.getElementById('cv_entered').style.display = 'block';
+		document.getElementById('cv').addEventListener('click', (ev) => {
+			ev.preventDefault();
+			window
+				.open()
+				.document.write(
+					`<iframe src="${atob(
+						response
+					)}" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; margin:0;" allowfullscreen></iframe>`
+				);
+		});
 	}
 });
